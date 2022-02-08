21e667f4950ea6867e756c95b5dc0038faad5d4c
mkuzminas <marius.kuzminas@gmail.com>
2022-02-08 19:10:22 +0200

    Memes not cars

---
+++ add/script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+
+const handleSubmitForm = (event) => {
+    event.preventDefault();
+    const title = document.querySelector("input[name=title]");
+    const subtitle = document.querySelector("input[name=subtitle]");
+    const src = document.querySelector("input[name=src]");
+
+    const meme = {
+        title: title.value,
+        subtitle: subtitle.value,
+        src: src.value,
+    };
+
+    fetch("http://localhost:3000/memes", {
+        method: "POST",
+        body: JSON.stringify(meme),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+};
+
+form.addEventListener("submit", handleSubmitForm);
--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const url = "https://olive-bead-glazer.glitch.me/"
-fetch(url)
+fetch("http://localhost:3000/memes")
     .then((response) => response.json())
-    .then((carsJson) => {
-        console.log(carsJson);
+    .then((memes) => {
+        console.log(memes);
+        renderMemes(memes);
+    })
+    .catch((error) => {
+        console.error("We got an error: ", error);
     });
+
+const renderMemes = (memes) => {
+    const container = document.querySelector(".container");
+
+    memes.forEach((meme) => {
+        const card = createMemeCard(meme);
+        container.append(card);
+    });
+};
+
+const createMemeCard = (meme) => {
+    const card = document.createElement("div");
+    const title = document.createElement("h3");
+    const imageContainer = document.createElement("div");
+    const image = document.createElement("img");
+    const subtitle = document.createElement("p");
+
+    title.innerText = meme.title;
+    image.src = meme.src;
+    image.alt = meme.title;
+    subtitle.innerText = meme.subtitle;
+
+    card.setAttribute("class", "card");
+    imageContainer.setAttribute("class", "image");
+
+    imageContainer.append(image);
+
+    card.append(title, imageContainer, subtitle);
+
+    return card;
+};
